4316791b1e11251a1765e7d36d4404ba994c6e02
PhucNGo <phucngo9856@gmail.com>
2021-01-14 19:25:05 +0700

    add feature

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import string
 import CodeTranslater
+import SudokuSolver
 
 if __name__ == '__main__' :
 
     print("----------------------------------------------------------------------------------------------------------------")
     print("--------------------------------------------------Made by Phien-------------------------------------------------")
-    print("Options: \n\n Encode Ceasar Code (1) \n\n Decode Ceasar Code (2) \n\n Encode VigenegeCode (3) \n\n Quit (9) \n\n")
+    print("Options: \n\n Encode Ceasar Code (1) \n\n Decode Ceasar Code (2) \n\n Encode VigenegeCode (3) \n\n SudokuSolver (4) \n\n Quit (9) \n\n")
     open = True
     while open :
         Option = int(input("Option: "))
@@ -25,5 +26,18 @@ if __name__ == '__main__' :
                 print("Error!!")
             else:
                 print("Decoded Vigenege Code:", CodeTranslater.DecodeVigenege(code, key))
+        if Option == 4:
+            columns = 9
+            rows = 9
+            table = []
+            for i in range (0,rows):
+                    table.append([])
+                    for j in range (0, columns):
+                        table[i].append(int(input()))
+            print("Board: ")
+            SudokuSolver.print_board(table)
+            SudokuSolver.solve(table)
+            SudokuSolver.print_board(table)
+
         
 
